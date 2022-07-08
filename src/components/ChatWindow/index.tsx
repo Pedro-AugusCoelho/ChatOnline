59d1ab7bb43f582de396dd72d1b7cfbe1f8bb187
Pedro-AugusCoelho <pe.augustocoelho@gmail.com>
2022-07-08 14:35:10 -0300

    Create ChatWindow Footer

--- src/components/ChatWindow/index.tsx
+++ src/components/ChatWindow/index.tsx
@@ -5,6 +5,10 @@ import styles from './styles.module.scss';
 import SearchIcon from '@mui/icons-material/Search';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import CloseIcon from '@material-ui/icons/Close';
+import SendIcon from '@material-ui/icons/Send';
+import MicIcon from '@material-ui/icons/Mic';
 
 export const ChatWindow = () => {
     return(
@@ -18,17 +22,56 @@ export const ChatWindow = () => {
                 </div>
                 
                 <div className={styles.HeaderBtns}>
+                    
                     <div className={styles.Btn}>
                         <SearchIcon style={{color:'#919191'}} />
+                    </div>
+                    
+                    <div className={styles.Btn}>
                         <AttachFileIcon style={{color:'#919191'}}/>
+                    </div>
+                    
+                    <div className={styles.Btn}>
                         <MoreVertIcon style={{color:'#919191'}}/>
                     </div>
+                
                 </div>
             
             </div>
             
             <div className={styles.chatWindowBody}></div>
-            <div className={styles.chatWindowFooter}></div>
+            
+            
+            
+            <div className={styles.chatWindowFooter}>
+                
+                <div className={styles.FooterPre}>
+
+                    <div className={styles.Btn}>
+                        <SearchIcon style={{color:'#919191'}} />
+                    </div>
+                
+                </div>
+                
+                <div className={styles.FooterInputArea}>
+                    <input 
+                        type='text' 
+                        className={styles.input} 
+                        placeholder='Digite uma menssagem'
+                    />
+                </div>
+                
+                <div className={styles.FooterPos}>
+                    
+                    <div className={styles.Btn}>
+                        <SendIcon style={{color:'#919191'}} />
+                    </div>
+                
+                </div>
+            
+            </div>
+        
+        
         </div>
     
     )
