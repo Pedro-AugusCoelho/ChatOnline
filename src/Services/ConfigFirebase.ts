bf91088d99d47419ec852eecd970bb62eabb04dc
Pedro A. Coelho Costa <peaugusto.coelho@gmail.com>
2022-07-20 17:20:12 -0300

    Delete ConfigFirebase.ts

--- src/Services/ConfigFirebase.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDsYOSXA7IjWPZZIojecS1GhDUgTQgtsMI",
-    authDomain: "chatonline-149b4.firebaseapp.com",
-    projectId: "chatonline-149b4",
-    storageBucket: "chatonline-149b4.appspot.com",
-    messagingSenderId: "655086345530",
-    appId: "1:655086345530:web:ac6e1d373f8dc851660403"
-};
